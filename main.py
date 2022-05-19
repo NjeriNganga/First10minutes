5adaf77ed0b5f3a7acf594be3827645824c4a08c
Lucy <lucyngng02@gmail.com>
2022-05-19 08:14:56 +0300

    First tutorial

---
+++ main.py
@@ -0,0 +1,9 @@
+full_name = input('What is your name? ')
+favorite_color = input('What is your favorite color? ')
+print(full_name + ' likes ' + favorite_color)
+birth_year=input('Birth_year: ')
+age= 2019 - int(birth_year)
+print(age)
+weight_lbs= input('weight(lbs): ')
+weight_kg = int(weight_lbs) * 0.45
+print(weight_kg)
